2fc986396d339ea2864473166fd5e82b62ae9877
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-09-16 01:05:14 -0400

    installed vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { StoreProvider } from '@/store/store-context';
 import '@/styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				<Component {...pageProps} />
 			</StoreProvider>
 			<footer>@2023 Kris Lui</footer>
+			<Analytics />
 		</>
 	);
 }

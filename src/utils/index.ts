ff30131438be6a1d7bb95bb64532ef72f5301245
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-09-15 23:13:35 -0400

    fixed isEmpty

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,22 +1,22 @@
 export const isEmpty = (obj: any): boolean => {
 	if (!obj) return true;
-	if (Object.keys(obj).length === 0) return true;
-	Object.values(obj).forEach((value) => {
-		switch (typeof value) {
-			case 'string': {
-				if (value !== '') return false;
-				break;
-			}
-			case 'number': {
-				if (value !== 0) return false;
-				break;
-			}
-			case 'object': {
-				if (!isEmpty(value)) return false;
-				break;
-			}
+
+	switch (typeof obj) {
+		case 'string': {
+			if (obj !== '') return false;
+			break;
 		}
-	});
+		case 'number': {
+			if (obj !== 0) return false;
+			break;
+		}
+		case 'object': {
+			for (const value of Object.values(obj)) {
+				if (!isEmpty(value)) return false;
+			}
+			break;
+		}
+	}
 
 	return true;
 };
